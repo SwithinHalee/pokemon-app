7b2ad62f94bc933734b78348b6b7eb6e896f65f3
SwithinHalee <143343934+SwithinHalee@users.noreply.github.com>
2026-01-09 08:33:07 +0700

    Feat: Added filtering for relevant file extensions and excluded system folders.

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,23 @@
 import type { NextConfig } from "next";
 
+const withPWA = require("next-pwa")({
+  dest: "public", // Folder tujuan file service worker
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development", // PWA hanya aktif saat production/deploy
+});
+
 const nextConfig: NextConfig = {
-  // --- KONFIGURASI GAMBAR ---
-  // Mengizinkan Next.js memuat gambar dari domain eksternal
+  // Config gambar (biarkan seperti sebelumnya)
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "raw.githubusercontent.com", // Server gambar Pokemon
-        pathname: "/PokeAPI/sprites/**",
+        hostname: "raw.githubusercontent.com",
       },
     ],
   },
 };
 
-export default nextConfig;
+// Bungkus config dengan withPWA
+export default withPWA(nextConfig);
